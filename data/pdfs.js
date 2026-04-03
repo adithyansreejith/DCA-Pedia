e4c68edbbf06042f2d7cf54f1b1ea4bea88fc1b4
Stallmanslave <123569961+adithyansreejith@users.noreply.github.com>
2026-04-03 12:59:15 +0530

    Update pdfs.js

--- data/pdfs.js
+++ data/pdfs.js
@@ -1,28 +1,38 @@
 const pdfData = {
     "msc-ai-ds": {
-        title: "MSc AI/DS Resources",
-        description: "Master of Science in Artificial Intelligence & Data Science",
-        folder: "Msc AI-DS",
-        files: [
-            { name: "Syllabus_MSc_AI_DS.pdf", size: "1.2 MB", date: "2024-03-20" },
-            { name: "Machine_Learning_Notes.pdf", size: "4.5 MB", date: "2024-03-25" }
+        "title": "MSc AI/DS Resources",
+        "description": "Master of Science in Artificial Intelligence & Data Science",
+        "folder": "Msc AI-DS",
+        "files": [
+            {
+                "name": "Syllabus.pdf",
+                "size": "24.0 B",
+                "date": "2026-04-03"
+            }
         ]
     },
     "mca": {
-        title: "MCA Resources",
-        description: "Master of Computer Applications",
-        folder: "MCA",
-        files: [
-            { name: "Syllabus_MCA_2024.pdf", size: "1.1 MB", date: "2024-03-20" },
-            { name: "Data_Structures_Algorithm.pdf", size: "3.8 MB", date: "2024-03-22" }
+        "title": "MCA Resources",
+        "description": "Master of Computer Applications",
+        "folder": "MCA",
+        "files": [
+            {
+                "name": "Syllabus.pdf",
+                "size": "24.0 B",
+                "date": "2026-04-03"
+            }
         ]
     },
     "integrated-mca": {
-        title: "Integrated MCA Resources",
-        description: "Integrated Master of Computer Applications",
-        folder: "Integrated MCA",
-        files: [
-            { name: "Integrated_MCA_Guide.pdf", size: "2.1 MB", date: "2024-03-15" }
+        "title": "Integrated MCA Resources",
+        "description": "Integrated Master of Computer Applications",
+        "folder": "Integrated MCA",
+        "files": [
+            {
+                "name": "Syllabus.pdf",
+                "size": "24.0 B",
+                "date": "2026-04-03"
+            }
         ]
     }
 };
